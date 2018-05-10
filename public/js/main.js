692b81a3bedf95a3aee983b76809f0d77f83973a
Timothy A Breeding <tim.breeding@yahoo.com>
2018-05-10 15:02:49 +0200

    More Stuff

--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,112 @@
+//Global Variables
 const spaces = document.querySelectorAll(".space");
-const plays = new Array(64).fill(0);
+const boardState =  [
+                        [0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0]
+                    ]
+const whosTurn = document.getElementById("turn");
 
-const white = 2;
+const loadCoordinates = () => {
+    let counter = 0;
+    for(let i = 0; i < Math.sqrt(spaces.length); i++) {
+        for(let j = 0; j < Math.sqrt(spaces.length); j++) {
+            spaces[counter].dataset.row = i;
+            spaces[counter].dataset.col = j;
+            counter++;
+        }
+    }
+}
+
+const empty = 0;
 const black = 1;
+const white = 2;
 
-console.log(spaces);
+let playerTurn; 
+
 const blackHtml = `<div class="circle playerTwo"></div>`;
 const whiteHtml = `<div class="circle playerOne"></div>`;
 
 const paintBoard = () => {
-    spaces.forEach((space, index) => {
-       switch(plays[index]) {
-            case 1:
-                space.innerHTML = blackHtml;
-                break;
-            case 2:
-                space.innerHTML = whiteHtml;
-                break;
-            default:
-                space.innerHTML = "";
-       }
-    });
-
+    let currSpace;
+    for(let i = 0; i < boardState.length; i++) {
+        for(let j = 0; j < boardState.length; j++) {
+            currSpace = document.querySelector("[data-col='" + j + "'][data-row='" + i + "']");
+            switch(boardState[i][j]) {
+                case 1:
+                    currSpace.innerHTML = blackHtml;
+                    break;
+                case 2:
+                currSpace.innerHTML = whiteHtml;
+                    break;
+                default:
+                currSpace.innerHTML = "";
+           }  
+        }
+    }
+    return;
 }
-const gameLoad = () => {
-    plays[27] = white;
-    plays[36] = white;
 
-    plays[28] = black;
-    plays[35] = black;   
+const changeTurn = (turn) => {
+    whosTurn = (black) ? white : black;
+    whosTurn.innerText = (turn == black) ? "Black" : "White";
+    return;
+}
+
+const search = (turn, square, stepRow, stepCol) => {
+    let col, row;
+    if(square.target.classList[0] == "space") {
+        col = parseInt(square.target.dataset.col);
+        row = parseInt(square.target.dataset.row);
+    } else {
+        col = parseInt(square.target.parentElement.dataset.col);
+        row = parseInt(square.target.parentElement.dataset.row);
+    }
+
+        let counter = 0;
+        if(!boardState[row][col]) {
+            // for (let i = 0; i < Math.sqrt(spaces.length); i++) {
+            while (col + stepCol <)    
+                if(boardState[row + stepRow][col + stepCol] !== turn && boardState[row + stepRow][col + stepCol]) {
+                    console.log("continue");
+                    counter++;
+                    stepRow += stepRow;
+                    stepCol += stepCol;
+                    } //else if (boardState[row + stepRow][col + stepCol] ) {}
+            }
+        // }
+}
+
+const spaceClickHandler = (e) => {
+
+    //Check to the right
+    search(playerTurn, e, 0, 1);
+}
+
+
+const gameLoad = () => {
+
+    boardState[3][3] = white;
+    boardState[4][4] = white;
+
+    boardState[3][4] = black;
+    boardState[4][3] = black;   
+
+    boardState[5][3] = white;
+
+    loadCoordinates();
     
     paintBoard();
-}
+    playerTurn = black;
+    whosTurn.innerText = "Black";
 
+    spaces.forEach(space => {
+        space.addEventListener("click", spaceClickHandler, false);
+    })
+}
 gameLoad();
