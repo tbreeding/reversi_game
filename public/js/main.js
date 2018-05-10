90b51b711c2c0db5821bf9ecfc2f0bba870d3aba
Timothy A Breeding <tim.breeding@yahoo.com>
2018-05-10 16:33:36 +0200

    Enough for today

--- public/js/main.js
+++ public/js/main.js
@@ -58,7 +58,8 @@ const changeTurn = (turn) => {
     return;
 }
 
-const search = (turn, square, stepRow, stepCol) => {
+const search = (turn, square, stepRow, stepCol, dir) => {
+    console.log(dir)
     let col, row;
     if(square.target.classList[0] == "space") {
         col = parseInt(square.target.dataset.col);
@@ -68,24 +69,70 @@ const search = (turn, square, stepRow, stepCol) => {
         row = parseInt(square.target.parentElement.dataset.row);
     }
 
-        let counter = 0;
-        if(!boardState[row][col]) {
-            // for (let i = 0; i < Math.sqrt(spaces.length); i++) {
-            while (col + stepCol <)    
-                if(boardState[row + stepRow][col + stepCol] !== turn && boardState[row + stepRow][col + stepCol]) {
-                    console.log("continue");
-                    counter++;
-                    stepRow += stepRow;
-                    stepCol += stepCol;
-                    } //else if (boardState[row + stepRow][col + stepCol] ) {}
-            }
+        let end = false;
+        let goodSpots = [];
+        if(boardState[row][col] === 0) {
+
+            let curRow = row + stepRow;
+            let curCol = col + stepCol;
+
+            while(!end) {
+                
+                if(boardState[curRow][curCol] !== turn && !!boardState[curRow][curCol]) {
+
+                    goodSpots.push([curRow, curCol])
+                    curRow += stepRow;
+                    curCol += stepCol;
+                    console.log("push", goodSpots);
+
+                } else if (curRow < 0 || 
+                    curRow >= Math.sqrt(spaces.length) || 
+                    curCol < 0 || 
+                    curCol >= Math.sqrt(spaces.length)) {
+
+                        playerTurn = turn == black ? white : black;
+                        console.log("end of board");
+                        whosTurn.innerText = (playerTurn === black) ? "Black" : "White";
+
+                        end = true;
+                } else {
+                    goodSpots.forEach(val => {
+                        boardState[val[0]][val[1]] = turn;
+                    })
+                    if(goodSpots.length > 0) {
+                        boardState[row][col] = turn;
+
+                        paintBoard();
+
+                        playerTurn = turn == black ? white : black;
+                        console.log("go");
+                        whosTurn.innerText = (playerTurn === black) ? "Black" : "White";
+                    } else {
+                        console.log("nope")
+                    }
+                    
+                    end = true;
+                }
+            } //else if (boardState[row + stepRow][col + stepCol] ) {}
+        }
         // }
+        return;
 }
 
 const spaceClickHandler = (e) => {
-
+    
     //Check to the right
-    search(playerTurn, e, 0, 1);
+    search(playerTurn, e, 0, 1, "right");
+    search(playerTurn, e, 0, -1, "left");
+    search(playerTurn, e, 1, 0, "down");
+    search(playerTurn, e, -1, 0, "up");
+
+    search(playerTurn, e, 1, 1, "down, right");
+    search(playerTurn, e, 1, -1, "down, left");
+    search(playerTurn, e, -1, 1, "up, right");
+    search(playerTurn, e, -1, -1, "up, left");
+
+    console.log("@@@@@@@ END @@@@@@")
 }
 
 
@@ -97,8 +144,9 @@ const gameLoad = () => {
     boardState[3][4] = black;
     boardState[4][3] = black;   
 
-    boardState[5][3] = white;
-
+    // boardState[3][5] = white; 
+    // boardState[3][6] = white; 
+    // boardState[3][7] = black; 
     loadCoordinates();
     
     paintBoard();
